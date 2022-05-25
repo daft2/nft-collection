4b2e67fc7f6c3f35d20ec7aa0ddfb75833f026af
daft2 <daftdevs@gmail.com>
2022-05-25 19:35:18 +0700

    feat: create constant

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x3Ae5134C220168D599e59F75319f4C890b664eDc";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
